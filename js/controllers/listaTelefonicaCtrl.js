c25590bc413cc46759b7f4b30fd8b2ba0861e217
jdc-santos <jdc_santos@outlook.com>
2021-04-03 14:50:56 -0300

    Organizando o projeto

---
+++ js/controllers/listaTelefonicaCtrl.js
@@ -0,0 +1,71 @@
+angular.module('listaTelefonica')
+  .controller('listaTelefonicaCtrl', function($scope, $http) {
+    
+    $scope.app = 'Lista Telefonica';
+    
+    // inicia com array vazio os contato e operadoras, para evitar erros
+    $scope.contatos = [];
+    $scope.operadoras = [];
+
+    // função para carregar os contatos da API
+    carregarContatos = function() {
+      $http.get('http://localhost:3000/contatos')
+      .then(function(res) {
+        $scope.contatos = res.data;
+      });
+    }
+
+    // função para carregar as operadoras da API
+    carregarOperadoras = function() {
+      $http.get('http://localhost:3000/operadoras')
+      .then(function(res) {
+        $scope.operadoras = res.data;
+      })
+    }
+
+    $scope.adicionarContato = function(contato) {
+      
+      $http.post('http://localhost:3000/contatos', contato)
+      .then(function(res) {
+        
+        // recarrega os dados em caso de sucesso
+        carregarContatos();
+        
+        // reseta os inputs do form
+        $scope.contatoForm.$setPristine();
+        
+        // deleta o objeto que foi criado para salvar o novo contato
+        delete $scope.contato;
+
+      }).catch(function(error) {
+
+        // mostra uma mensagem para o usuário
+        $scope.message = error.data;
+
+        // mostra o erro no console
+        console.log(error);
+      });
+
+    }
+
+    $scope.apagarContatos = function(contatos) {
+      $scope.contatos = contatos.filter(function(e) {
+        return !e.selecionado;
+      });
+    }
+
+    $scope.isContatoSelecionado = function(contatos) {
+      return contatos.some(function(contato){
+        return contato.selecionado;
+      });
+    }
+
+    $scope.ordenarPor = function(campo) {
+      $scope.criterioDeOrdenacao = campo;
+      $scope.direcaoOrdenacao = !$scope.direcaoOrdenacao;
+    }
+
+    // carrega os contatos e operadoras ao carregar a página
+    carregarContatos();
+    carregarOperadoras();
+  });
